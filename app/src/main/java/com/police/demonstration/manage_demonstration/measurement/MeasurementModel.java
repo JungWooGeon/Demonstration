449c5ad810beb0b1a7583dcdcc56f933d5dfb3fb
JungWooGeon <jwk1734@naver.com>
2023-06-15 17:42:48 +0900

    add color

---
+++ app/src/main/java/com/police/demonstration/manage_demonstration/measurement/MeasurementModel.java
@@ -0,0 +1,5 @@
+package com.police.demonstration.manage_demonstration.measurement;
+
+public class MeasurementModel {
+
+}
--- app/src/main/java/com/police/demonstration/manage_demonstration/measurement/MeasurementViewModel.java
+++ app/src/main/java/com/police/demonstration/manage_demonstration/measurement/MeasurementViewModel.java
@@ -3,5 +3,5 @@ package com.police.demonstration.manage_demonstration.measurement;
 import androidx.lifecycle.ViewModel;
 
 public class MeasurementViewModel extends ViewModel {
-
+    MeasurementModel model = new MeasurementModel();
 }
